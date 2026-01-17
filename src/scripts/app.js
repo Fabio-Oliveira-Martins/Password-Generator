04d0c998e72edf61264c00c7b92ef6b26cb53b11
Fabio-Oliveira-Martins <fabiooliveiramartins@outlook.com>
2026-01-17 19:03:26 -0300

    Fixed code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,26 +1,31 @@
 import { PasswordGenerator } from "./password_generator.js";
 import { copied_alert } from "./components/copied_alert.js"
 
-
+// ===== COMPONENTE ===== //
 const passwordGenerator = document.querySelector('.password-generator');
 
+// ===== PEGAR EVENTO DE CLICK DENTRO DO COMPONENTE ===== //
 passwordGenerator.addEventListener('click', e => {
     const el = e.target;
 
+    // ===== BOTÃO COPIAR ===== //
     if (el.classList.contains("password-generator__copy")) {
         const display = passwordGenerator.querySelector('.password-generator__display');
         
+        // ----- COPIAR SE INPUT NÃO ESTIVER VAZIO ----- //
         if(display.value) {
             navigator.clipboard.writeText(display.value);
             copied_alert();
         }
     }
 
+    // ===== BOTÃO GERAR NOVA SONHA ===== //
     if(el.classList.contains("password-generator__generate-password")) {
         const display = passwordGenerator.querySelector('.password-generator__display');
 
-       const newPassword = new PasswordGenerator;
-       display.value = newPassword.createPassword();
+        // ----- PEGAR SENHA GERADA ----- //
+        const newPassword = new PasswordGenerator;
+        display.value = newPassword.createPassword();
     }
 });
 
--- src/scripts/password_generator.js
+++ src/scripts/password_generator.js
@@ -5,27 +5,24 @@ export class PasswordGenerator {
         this.numbers = `0123456789`;
         this.symbols = `!@#$%^&*()_+-=[]{}|;':",./<>?~`;        
         this.allChar = this.lower + this.upper + this.numbers + this.symbols;
-        this.charLimit = 14;
+        this.charLimit = 5;
         this.charList = [];
     }
 
+    // ===== GERAR NÚMERO ALEATÓRIO ===== //
     randomNumber() {
-        const min = 0;
-        const max = this.allChar.length;
-        return Math.floor(Math.random() * (max - min + 1)) + min;
+        return Math.floor(Math.random() * this.allChar.length);
     }
 
+    // ===== GERAR SENHA ALEATÓRIA ===== //
     createPassword() {
         for(let i = 0; i < this.charLimit; i++) {
             const random = this.randomNumber();
 
             this.charList.push(this.allChar[random]);
-            
-            if(i === this.charLimit) {
-                break;
-            }
         }
 
+        // ----- RETORNAR A SENHA CONCATENADA ----- //
         return this.charList.join("");
     }
 }
